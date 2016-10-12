a14a1242cdf670f681bb8450247eda7d2367840b
Nick <nicknchambers@gmail.com>
2016-10-11 18:26:53 -0600

    connects api with index

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
 require('./main');
+
+function getWeather() {
+  $.get('http://weatherly-api.herokuapp.com/api/weather', function(response) {
+    debugger;
+    var weather = JSON.parse(response);
+    appendWeather();
+  })
+}
+
+ $('.btn-input').on('click', function(){
+  getWeather();
+});
+
+function appendWeather(weather) {
+  $('.weather-display').append(`<p> ${ weather }</p>`);
+};
--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,8 @@
 console.log("HOLA MUNDO");
+
+//getting value from input and talking to api and then render
+// $.ajax({
+//   method: 'GET',
+//   dataType: 'json',
+//   url: 'http://weatherly-api.herokuapp.com/api/weather'
+// }),
