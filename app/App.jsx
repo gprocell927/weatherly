5b01bad3e2e8108ff9569d754ce2ba56ce74c4ee
Nick <nicknchambers@gmail.com>
2016-10-13 13:07:25 -0600

    debugs enzyme setup and starts to debug enzyme tests

--- app/App.jsx
+++ app/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Jquery from 'jquery';
 import InputField from './Inputfield';
+const $ = require('jquery');
+
 
 //getting value from input and talking to api and then render
 // $.ajax({
@@ -18,17 +20,17 @@ const App = React.createClass({
     }
   },
   // this code will run after the component is mounted to the DOM
-  componentDidMount() {
-    $.get('http://weatherly-api.herokuapp.com/api/weather', (response) => {
-      this.setState({
-        weather: response
-      })
-
-    })
-
-    // 1) fetch your ajax things
-    // 2) setState with the response
-  },
+  // componentDidMount() {
+  //   $.get('http://weatherly-api.herokuapp.com/api/weather', (response) => {
+  //     this.setState({
+  //       weather: response
+  //     })
+  //
+  //   })
+  //
+  //   // 1) fetch your ajax things
+  //   // 2) setState with the response
+  // },
 
   setLocation: function(location) {
 
--- test/unit-test.js
+++ test/unit-test.js
@@ -1,10 +1,18 @@
 require ('./index.js');
+require('./helpers/setup.js');
+require('./app-test.js')
+const React  = require('react')
+var expect = require('chai').expect
+const App = require('../app/App')
+import {shallow, mount, render } from 'enzyme'
+const $ = require ('jquery')
+
 
 describe('input attributes', function(){
-  it('should recognize when a zipcode has been entered', function(){
-
-      var zip = checkInputType(80013);
-
-      assert.equal(zip.length, 5);
+  it('should have an input field', function(){
+      const wrapper = shallow (
+        <App/>
+      )
+      expect(wrapper.contains(<InputField />).to.be.true)
   });
 });
