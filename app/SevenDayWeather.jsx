4df4250f0f042ebf13ca11f91c473c10c9c117be
Gabi Procell <gprocell927@gmail.com>
2016-10-18 11:28:31 -0600

    WIP sass styling

--- app/SevenDayWeather.jsx
+++ app/SevenDayWeather.jsx
@@ -3,11 +3,12 @@ import React, { PropTypes } from 'react';
 const SevenDayWeather = (props) => {
   const { weatherItem } = props;
     return (
-      <article key={weatherItem.date} className='tomorrowsWeather'>
-        <p>{weatherItem.weatherType.type}</p>
+      <section key={weatherItem.date} className= 'tomorrowsWeather'>
+        <p>{weatherItem.date}</p>
+        <p className={weatherItem.weatherType.type + 'lol'}>{weatherItem.weatherType.type}</p>
         <p>{weatherItem.temp.high}&deg;</p>
         <p>{weatherItem.temp.low}&deg;</p>
-      </article>
+      </section>
     );
 };
 
